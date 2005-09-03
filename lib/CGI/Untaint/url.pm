947b09949d6c1130b231df0aff45bef9af561902
Tony Bowden <tony@tmtm.com>
2005-09-03 19:38:36 +0000

    Import of TMTM/CGI-Untaint-url-1.00 from CPAN.
    gitpan-cpan-distribution: CGI-Untaint-url
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         TMTM/CGI-Untaint-url-1.00.tar.gz
    gitpan-cpan-author:       TMTM
    gitpan-cpan-maturity:     released

--- lib/CGI/Untaint/url.pm
+++ lib/CGI/Untaint/url.pm
@@ -1,21 +1,24 @@
 package CGI::Untaint::url;
 
+$VERSION = '1.00';
+
 use strict;
 use base 'CGI::Untaint::printable';
-use URI::Find;
-
-use vars qw/$VERSION/;
-$VERSION = '0.03';
+use URI::Find::Schemeless::Stricter;
 
 sub is_valid {
-  my $self = shift;
-  my $value = $self->value or die "No value\n";
-  my @urls;
-  find_uris($value, sub { push @urls, shift });
-  return $self->value($urls[0]) if @urls;
-  return;
+	my $self = shift;
+	my $value = $self->value or die "No value\n";
+	my @urls;
+	our $finder = URI::Find::Schemeless::Stricter->new(
+		sub {
+			push @urls, shift;
+		}
+	);
+	$finder->find(\$value);
+	return $self->value($urls[0]) if @urls;
+	return;
 }
-
 
 =head1 NAME
 
@@ -30,11 +33,13 @@ CGI::Untaint::url - validate a URL
 
 =head1 DESCRIPTION
 
+=head2 is_valid
+
 This Input Handler verifies that it is dealing with a reasonable
 URL. This mostly means that it will find the first thing that looks
 like a URL in your input, where by "looks like", we mean anything that
-URI::URL thinks is sensible, (with some tweaks, courtesy of URI::Find),
-so it will accept any of (for example):
+URI::URL thinks is sensible, (with some tweaks, courtesy of
+URI::Find::Schemeless::Stricter), so it will accept any of (for example):
 
   http://c2.com/cgi/wiki
   www.tmtm.com
@@ -42,25 +47,32 @@ so it will accept any of (for example):
   [http://www.angelfire.com/la/carlosmay/Tof.html]
   ftp://ftp.ftp.org/
 
-L<URI::URL> for much more information here.
-
-The resulting value will be a URI::URL object. 
-L<URI::URL> for more information on this.
+The resulting value will be a L<URI::URL> object. 
 
 =head1 SEE ALSO
 
-L<URI::URL>. L<URI::Find>.
+L<URI::URL>. L<URI::Find::Schemeless::Stricter>.
 
 =head1 AUTHOR
 
-Tony Bowden, E<lt>kasei@tmtm.comE<gt>. 
+Tony Bowden
+
+=head1 BUGS and QUERIES
+
+Please direct all correspondence regarding this module to:
+  bug-CGI-Untaint-url@rt.cpan.org
 
 =head1 COPYRIGHT
 
-Copyright (C) 2001 Tony Bowden. All rights reserved.
+  Copyright (C) 2001-2005 Tony Bowden.
 
-This module is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License; either version 2 of the License,
+  or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.
 
 =cut
 
